e020a19e422b40df07cacc6a28924c5174e7e86e
Tharun1756 <158495832+Tharun1756@users.noreply.github.com>
2024-02-01 21:48:24 +0530

    Add files via upload

---
+++ java files/JavaPractics/src/javaprogram/javaDog.java
@@ -0,0 +1,110 @@
+package javaprogram;
+
+// veraible is a small container and we can story the data from the values ;
+// verabiles they have 4 types 
+// 1. instant veriable (non-static field), 2. class veriable (static field),3.local veriable , 4.peremeter 
+
+
+public class javaDog {
+	
+
+		
+	
+	
+// 1. instant veriable (non-static field);
+	
+//	String dogname = "max";
+//	int hight = 40;
+//	double dogcost = 20.000;
+	
+	
+	// 2. class veraible (static field);
+	
+//	static double lenght = 12.9;
+//	static String color = "black";
+//	static int   lifetime = 20;
+	
+	
+
+	public static void main(String[] args) {
+		
+		
+//		
+//	1.	javaDog a = new javaDog();
+//		System.out.println(a.dogname);
+//		
+//		javaDog b = new javaDog();
+//		System.out.println(b.hight);
+//		 
+//		javaDog c = new javaDog();
+//		System.out.println(c.dogcost);
+//		
+		
+		// two ways in creating the the object
+		
+
+//		System.out.println(new javaDog().dogname );
+//		System.out.println(new javaDog().hight);
+//	1.	System.out.println(new javaDog ().dogcost);
+		
+		
+		
+		//2.class veriable static field;
+		
+// 2.	System.out.println(lenght);
+//	System.out.println(color);
+//	System.out.println(lifetime);
+	
+//		javaDog v = new javaDog();
+//		System.out.println(v.lenght);
+//		
+//		
+//		javaDog t = new javaDog();
+//	System.out.println(t.color);
+//	
+//	javaDog s = new javaDog();
+// 2.	System.out.println(s.lifetime);
+		
+		
+		
+		// 3. local veriable 
+		
+		// local veriable are used to in only methods and local members 
+		
+		 //int weite = 50;
+		// String bread = "italian";
+		 
+//		 System.out.println(weite);
+//		 System.out.println(bread);
+		 
+	//3.	 //System.out.println( new javaDog().weite); not work in local veriable in object crection
+		
+		 
+
+		// 4. peremeters 
+	 
+	   // javaDog a = new javaDog(int a);
+
+
+    // *&^%$  veraible are use two 1. veriable declerection , 2. veriable inilizection 
+		
+		// 1. veriable declerection
+		
+	// syntex :- datatype variable declerection = veriable inilizection
+		
+		
+		 // int a; this is veriable declerection 
+		
+		// int a =20 ; is veriable inilizetion
+		
+		
+			
+			// @#$%  veriable re used to default values auto metical add in compiler 
+		
+		int t = 3400;
+		t=4500;
+		System.out.println(t);
+		
+	}
+
+}
